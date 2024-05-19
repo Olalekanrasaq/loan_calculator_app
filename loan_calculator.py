02b246d0da0ede16bb3c2843fcc581bc7a56199a
Olalekan Rasaq <69814366+Olalekanrasaq@users.noreply.github.com>
2024-05-19 20:45:03 +0100

    Update loan_calculator.py

--- loan_calculator.py
+++ loan_calculator.py
@@ -39,7 +39,7 @@ loan_amount = st.sidebar.text_input('Loan Amount (#)', '0')
 loan_amount = int(loan_amount.replace(',', ''))
 
 # input daily interest rate in a float format
-daily_interest_rate = st.sidebar.number_input('Daily Interest Rate (%)', min_value=0.0, value=0.1)
+daily_interest_rate = st.sidebar.number_input('Daily Interest Rate (%)', min_value=0.000, value=0.100)
 
 # calculate the monthly interest rate
 monthly_interest_rate = daily_interest_rate * 30
